6544adfd5963eb3996a3771b68127874ffb418a2
Andreas Gohr <andi@splitbrain.org>
2014-08-21 19:23:42 +0000

    added controller to run the whole process

---
+++ scan.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+JOBID=`date '+%Y-%m-%d_%H%M%S'`
+USER=$1
+KEYWORD=$2
+
+if [ -z "$USER"]; then
+    echo "Usage: $0 <user> [<keyword>]"
+    echo "please give a user"
+    exit 1
+fi
+
+
+# run the scanning in foreground
+$DIR/01-scan.sh "$JOBID"
+
+# execute processing in background
+(
+    # lock processing to make sure only one is running at a time
+    (
+        flock -x 200 # wait for lock
+
+        $DIR/02-createpdf.sh "$JOBID"
+        $DIR/03-nascopy.sh "$JOBID" "$USER" "$KEYWORD"
+        $DIR/04-gdrivecopy.sh "$JOBID" "$USER" "$KEYWORD"
+
+    ) 200>/tmp/scan.lock
+) &
+
+
